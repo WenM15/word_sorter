688165f5e92ea3533868e33cd593055ee86b16eb
WenM15 <wenmingleong01@gmail.com>
2023-08-08 00:09:56 +0800

    Added a word storage

--- word_sorter.cpp
+++ word_sorter.cpp
@@ -3,15 +3,32 @@
 using namespace std;
 
 int main() {
+	// Create a pointer to point to an array of pointers
+	char** listPointer = nullptr;
 
 	bool redo = true;
 	while (redo) {
 		// Input
 		cout << "Input: " << endl;
 
+		// Define starting memory size for list
+		size_t listSize = 10;
+		
+		// Preallocate memory for 10 words
+		listPointer = (char**)::operator new(listSize * sizeof(char*));//
+		
+		// Set a starting wordIndex for list
+		int wordIndex = 0;
+		//Set a starting charIndex for each word
+		int charIndex = 0;
+
+		// Define starting memory size for words
+		size_t wordSize = 10;
+		
 		bool isSpace = false;
 		bool extractionDone = false;
 		while (!extractionDone) {
+
 			// Extraction
 			char input;
 			cin.get(input);
@@ -19,32 +36,54 @@ int main() {
 			// Filtering before storage
 			if ((input >= 'a' && input <= 'z') || (input >= 'A' && input <= 'Z')) {
 				if (isSpace) {
-					cout << "There's a space before this char" << endl;
-					// Add word to list
+					++wordIndex;
+					if (wordIndex == listSize) {
+						// Allocate more memory for list
+					}
+					charIndex = 0;
+
 				}
-				else {
-					cout << "There's not a space before this char" << endl;
-					// Continue adding characters
+				// Preallocate memory for 10 characters
+				if (charIndex == 0) {
+					listPointer[wordIndex] = (char*)::operator new(wordSize * sizeof(char));//
 				}
+
+				listPointer[wordIndex][charIndex] = input;
+				cout << wordIndex << " " << charIndex << " " << listPointer[wordIndex][charIndex] << endl;
+				++charIndex;
+				if (charIndex == wordSize) {
+					// Allocate more memory for word
+				}
+
 				isSpace = false;
 			}
 			else if (input == ' ' || input == '\t') {
-				if (isSpace) {
-					cout << "There is a space before this space" << endl;
-					isSpace = true;
-					// Do nothing
+				// End of word, add \0 to the character array
+				if (!isSpace) {
+					listPointer[wordIndex][charIndex] = '\0';
 				}
-				else {
-					cout << "There is a letter before this space" << endl;
-					isSpace = true;
-					// End of word, add \0 to the character array
-				}
+				isSpace = true;
 			}
 			else if (input == '\n') {
+				// End of word, add \0 to the character array
+				if (!isSpace) {
+					listPointer[wordIndex][charIndex] = '\0';
+				}
+				// Add a '\0' to first character of a new c-string to indicate the end of the list
+				++wordIndex;
+				if (wordIndex == listSize) {
+					// Allocate more memory for list
+				}
+				// Preallocate memory for 10 characters
+				listPointer[wordIndex] = (char*)malloc(wordSize * sizeof(char));//
+
+				// Creating a word that only has '\0', this word will denote the end of the list
+				listPointer[wordIndex][0] = '\0';
+				
 				extractionDone = true;
 				redo = false;
 			}
-			else  {
+			else {
 				// Clear the buffer
 				bool newlineFound = false;
 				while (!newlineFound) {
@@ -57,8 +96,15 @@ int main() {
 				// Clear storage here
 				extractionDone = true;
 			}
-		
+
 		}//while(!extractionDone)
 	}//while(redo)
 
+	for (int wordIndex = 0; listPointer[wordIndex][0] != '\0'; ++wordIndex) {
+		for (int charIndex = 0; listPointer[wordIndex][charIndex] != '\0'; ++charIndex) {
+			cout << "listPointer[" << wordIndex << "][" << charIndex << "]: " << listPointer[wordIndex][charIndex] << ", ";
+		}
+		cout << endl;
+	}
+	
 }//main()
